736f2d6f234ed318360ad085f40833d3386da7d5
tricarte <sonagirbas@gmail.com>
2026-08-26 12:53:09 +0300

    fix nvim socket path

--- open_with_nvim.sh
+++ open_with_nvim.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 for file in "${@}"; do
+  # Skip binary files
+  # FIXME: returns binary even for empty text files
+  # ftype="$(file --mime-encoding ${f} | cut -d ' ' -f2)"
+  # if [[ "${ftype}" =~ binary$ ]]; then
+  #   continue
+  # fi
   file=$(realpath "${file}")
   /opt/nvim-linux-x86_64/bin/nvim --noplugin \
-    --cmd "lua vim.fn.rpcrequest(vim.fn.sockconnect('pipe', '/tmp//nvim.server.pipe', { rpc = true }), 'nvim_command', ':e! ${file}')" \
+    --cmd "lua vim.fn.rpcrequest(vim.fn.sockconnect('pipe', '/tmp/nvim.server.pipe', { rpc = true }), 'nvim_command', ':e! ${file}')" \
     --cmd "q" --headless
 done
